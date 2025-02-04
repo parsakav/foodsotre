fe829104eafd2c0350fae8a7218815fb100a0279
Parsa Kavianpour <parsakavianpour@gmail.com>
2025-02-04 22:43:14 +0330

    Corse disable

--- src/main/java/org/example/sotre/config/WebConfig.java
+++ src/main/java/org/example/sotre/config/WebConfig.java
@@ -2,16 +2,15 @@ package org.example.sotre.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**");
+        //    .allowCredentials(true);
     }
 }
--- src/main/java/org/example/sotre/config/security/SecurityConfig.java
+++ src/main/java/org/example/sotre/config/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.example.sotre.config.security;
 
 import org.apache.tomcat.util.bcel.Const;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -19,6 +20,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
@@ -34,10 +40,22 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean(name = "source")
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/", configuration);
+		return source;
+	}
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http, AuthenticationProvider authProvider,
-			AuthenticationManager authenticationManager, UserDetailsService userDetailsService) throws Exception {
-
+									AuthenticationManager authenticationManager, @Qualifier("source") CorsConfigurationSource source, UserDetailsService userDetailsService) throws Exception {
+		http.cors(e->{
+			e.configurationSource(source);
+		});
 		http.authenticationProvider(authProvider);
 http.csrf(AbstractHttpConfigurer::disable);
 
